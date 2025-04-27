717fda49edbe553290a65d45f9177632b1c497e4
Muhamad690 <haritsbawazier@gmail.com>
2025-04-27 14:22:53 +1200

    attack system for player and enemy

---
+++ Assets/Scripts/Combat/Unit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHp;
+    public int currentHp;
+
+
+    //taking damage 
+    public bool TakeDamage(int dmg)
+    {
+        currentHp -= dmg;
+
+        if (currentHp <= 0)
+            return true;
+        else
+            return false;
+    }
+}
