29e76ddce23f94116b2e5f1660adf8757b9cdbcd
LeoAUT1 <vcs4803@autuni.ac.nz>
2025-06-03 15:40:23 +1200

    Update Enemy.cs
    Set it back to what it was (unimportant)

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Enemy : CombatEntity
 {
     [SerializeField] protected int experienceGranted; //experience given to the player when they defeat this enemy
-
+    
     public int getExperience() { return experienceGranted; } //Not sure if we need this
+
 }
+
