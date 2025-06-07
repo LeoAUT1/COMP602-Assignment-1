fd78f06b5f6b8367d493ed5194b7f38879ba6bb1
TEDWAP <teddy.xu0524@gmail.com>
2025-06-07 17:31:46 +1200

    Tests successful: EncounterManager & PowerupData tests all green, ready for Matt to review

--- Assets/EditorTests/EncounterManagerTests.cs
+++ Assets/EditorTests/EncounterManagerTests.cs
@@ -1,86 +1,74 @@
+using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
-using System.Reflection; // Added for BindingFlags clarity
 
 namespace EditorTests
 {
     public class EncounterManagerTests
     {
         private EncounterManager encounterManager;
-        private EncounterData[] createdMockEncounters; // To keep track for cleanup
+        private EncounterData[] mockEncounters;
 
         [SetUp]
         public void Setup()
         {
-            // Setup EncounterManager (assuming it's still a MonoBehaviour)
+            // create EncounterManager mono
             var go = new GameObject("EncounterManagerGO");
             encounterManager = go.AddComponent<EncounterManager>();
 
-            // create mock encounter
-            EncounterData[] mockEncounters = new EncounterData[3];
+            // build n store our fake EncounterData array
+            mockEncounters = new EncounterData[3];
             for (int i = 0; i < mockEncounters.Length; i++)
             {
-                // No GameObject needed for ScriptableObject instantiation
                 var data = ScriptableObject.CreateInstance<EncounterData>();
                 data.encounterName = $"Test {i}";
-                // Optionally, ScriptableObjects also have a 'name' property you might want to set for debugging:
-                // data.name = $"TestEncounterSO_{i}";
-                createdMockEncounters[i] = data;
+                mockEncounters[i] = data;
             }
 
-            // use reflection to assign the private field
-            typeof(EncounterManager)
-                .GetField("encounters", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.SetValue(encounterManager, mockEncounters);
+            // inject the private encounter
+            var field = typeof(EncounterManager)
+                .GetField("encounters", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.NotNull(field, "Could not find private field 'encounters' on EncounterManager");
+            field.SetValue(encounterManager, mockEncounters);
         }
 
         [Test]
-        public void GetRandomEncounters_ReturnsCorrectCount()
+        public void GetRandomEncounters_ReturnsRequestedCount()
         {
             var result = encounterManager.GetRandomEncounters(5);
-
-            Assert.IsNotNull(result, "Returned array should not be null");
+            Assert.NotNull(result, "Returned array should not be null");
             Assert.AreEqual(5, result.Length, "Should return exactly 5 encounters");
         }
 
         [Test]
-        public void GetRandomEncounters_HandlesRequestLargerThanPool()
+        public void GetRandomEncounters_LoopsWhenCountExceedsPool()
         {
             var result = encounterManager.GetRandomEncounters(10);
-
-            Assert.IsNotNull(result);
+            Assert.NotNull(result);
             Assert.AreEqual(10, result.Length);
         }
 
         [Test]
-        public void GetRandomEncounters_ShufflesContent()
+        public void GetRandomEncounters_VariesOrderBetweenCalls()
         {
-            var result1 = encounterManager.GetRandomEncounters(3);
-            var result2 = encounterManager.GetRandomEncounters(3);
-
-            Assert.AreNotEqual(result1[0], result2[0], "Shuffled encounters should differ between calls");
+            var first  = encounterManager.GetRandomEncounters(3);
+            var second = encounterManager.GetRandomEncounters(3);
+            Assert.AreNotEqual(first[0], second[0],
+                "Expected first element to differ between two shuffled calls.");
         }
 
         [TearDown]
-        public void Cleanup()
+        public void Teardown()
         {
-            // Destroy the GameObject for EncounterManager
-            if (encounterManager != null && encounterManager.gameObject != null)
-            {
+            // destroy manager
+            if (encounterManager != null)
                 Object.DestroyImmediate(encounterManager.gameObject);
-            }
 
-            // Destroy the created ScriptableObject instances
-            if (createdMockEncounters != null)
+            // clean up our ScriptableObjects
+            if (mockEncounters != null)
             {
-                for (int i = 0; i < createdMockEncounters.Length; i++)
-                {
-                    if (createdMockEncounters[i] != null)
-                    {
-                        Object.DestroyImmediate(createdMockEncounters[i]);
-                    }
-                }
-                createdMockEncounters = null; // Clear the reference
+                foreach (var e in mockEncounters)
+                    Object.DestroyImmediate(e);
             }
         }
     }
